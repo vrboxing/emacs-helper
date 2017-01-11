85ba6c40f9924509b0a7a7d7401426fe65cb89d8
Feng Shu <tumashu@163.com>
2017-01-12 04:26:29 +0800

    Remove ebib-handy, it has been broken

--- eh-misc.el
+++ eh-misc.el
@@ -288,13 +288,6 @@
     (("C-x F" . projectile-find-file)
      ("C-S-s" . projectile-grep))))
 
-;; ebib-handy
-(use-package org
-  :config
-  (use-package ebib-handy
-    :config
-    (ebib-handy-enable)))
-
 ;; undo tree
 (use-package undo-tree
   :bind (("C-c /" . undo-tree-visualize))
