cb98c831047dc6bec73b0950c52d3f6f8e9f97e6
Feng Shu <tumashu@163.com>
2017-07-10 18:51:13 +0800

    Update emms config

--- eh-emms.el
+++ eh-emms.el
@@ -139,23 +139,21 @@
      (not (funcall (emms-browser-filter-only-recent 30) track))))
 
   ;; 设置 emms browser 和 browser-playlist 中音乐的显示格式
-  (setq emms-browser-current-indent "")
   (setq emms-browser-info-artist-format "* %n")
   (setq emms-browser-info-album-format  "   - %n")
   (setq emms-browser-info-title-format  "     ♪ %n")
-  (setq emms-browser-playlist-info-title-format "♪ %n")
+  (setq emms-browser-playlist-info-artist-format "* %n")
+  (setq emms-browser-playlist-info-album-format  "   - %n")
+  (setq emms-browser-playlist-info-title-format  "     ♪ %n")
 
-  ;; 禁止在 playlist 中插入 group 信息。
-  (advice-add 'emms-browser-playlist-insert-bdata
-              :override #'eh-emms-browser-playlist-insert-bdata)
-
-  (defun eh-emms-browser-playlist-insert-bdata (bdata starting-level)
-    (let ((type (emms-browser-bdata-type bdata))
-          (name (emms-browser-bdata-name bdata)))
-      (dolist (item (emms-browser-bdata-data bdata))
-        (if (not (eq type 'info-title))
-            (emms-browser-playlist-insert-bdata item starting-level)
-          (emms-browser-playlist-insert-track bdata))))))
+  ;; Set fontsize of face.
+  (dolist (face '(emms-browser-year/genre-face
+                  emms-browser-artist-face
+                  emms-browser-composer-face
+                  emms-browser-performer-face
+                  emms-browser-album-face
+                  emms-browser-track-face))
+    (set-face-attribute face nil :height 1.0)))
 
 ;; * Footer
 (provide 'eh-emms)
