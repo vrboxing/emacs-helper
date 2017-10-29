ec0ab56a27de146f1330bdfc7d97b77ad6152401
Feng Shu <tumashu@163.com>
2017-10-29 20:27:04 +0800

    Add eh-create-org-holiday-file

--- eh-org.el
+++ eh-org.el
@@ -181,117 +181,147 @@
                                 (format "，第%02d周" iso-week)
                               ""))))
         (format "%04d-%02d-%02d %s %s"
-                year month day dayname extra)))
+                year month day dayname extra))))
 
-    (use-package holidays
+  (use-package holidays
+    :ensure nil
+    :config
+    (defvar eh-calendar-holidays nil)
+    (defvar eh-calendar-personal nil)
+
+    (defun eh-create-org-holiday-file ()
+      "从 holiday 设置中生成一个 holiday org 文件，主要用于 orgzly."
+      (interactive)
+      (let* ((org-file (concat (file-name-as-directory
+                                eh-org-directory)
+                               "i-other.org"))
+             (start-year (calendar-extract-year
+                          (calendar-current-date)))
+             (end-year (+ start-year 1))
+             (calendar-holidays
+              (append eh-calendar-personal eh-calendar-holidays))
+             (s (calendar-absolute-from-gregorian (list 2 1 start-year)))
+             (e (calendar-absolute-from-gregorian (list 11 1 end-year)))
+             (displayed-month 2)
+             (displayed-year start-year)
+             holiday-list)
+        (while (<= s e)
+          (setq holiday-list (append holiday-list (calendar-holiday-list)))
+          (calendar-increment-month displayed-month displayed-year 3)
+          (setq s (calendar-absolute-from-gregorian
+                   (list displayed-month 1 displayed-year))))
+        (with-temp-buffer
+          (insert "### Auto generated, don't edit this file by hand!!! ###\n\n")
+          (insert
+           (mapconcat (lambda (x)
+                        (format "* TODO %s\n  SCHEDULED: <%s>"
+                                (cadr x)
+                                (let ((calendar-date-display-form
+                                       '((format "%s-%.2d-%.2d %s" year
+                                                 (string-to-number month)
+                                                 (string-to-number day)
+                                                 dayname))))
+                                  (calendar-date-string (car x)))))
+                      holiday-list "\n"))
+          (write-file org-file))))
+
+    (use-package cal-china-x
       :ensure nil
       :config
-      (use-package cal-china-x
-        :ensure nil
-        :config
-        (defvar eh-calendar-holidays nil)
-        (setq eh-calendar-holidays
-              '(;;公历节日
-                (holiday-fixed 1 1 "元旦")
-                (holiday-fixed 2 14 "情人节")
-                (holiday-fixed 3 8 "妇女节")
-                (holiday-fixed 3 14 "白色情人节")
-                (holiday-fixed 4 1 "愚人节")
-                (holiday-fixed 5 1 "劳动节")
-                (holiday-fixed 5 4 "青年节")
-                (holiday-float 5 0 2 "母亲节")
-                (holiday-fixed 6 1 "儿童节")
-                (holiday-float 6 0 3 "父亲节")
-                (holiday-fixed 9 10 "教师节")
-                (holiday-fixed 10 1 "国庆节")
-                (holiday-fixed 10 24 "程序员节")
-                (holiday-fixed 12 25 "圣诞节")
-                ;; 农历节日
-                (holiday-lunar 1 1 "春节" 0)
-                (holiday-lunar 1 2 "春节" 0)
-                (holiday-lunar 1 3 "春节" 0)
-                (holiday-lunar 1 15 "元宵节" 0)
-                (holiday-solar-term "清明" "清明节")
-                (holiday-solar-term "小寒" "小寒")
-                (holiday-solar-term "大寒" "大寒")
-                (holiday-solar-term "立春" "立春")
-                (holiday-solar-term "雨水" "雨水")
-                (holiday-solar-term "惊蛰" "惊蛰")
-                (holiday-solar-term "春分" "春分")
-                (holiday-solar-term "谷雨" "谷雨")
-                (holiday-solar-term "立夏" "立夏")
-                (holiday-solar-term "小满" "小满")
-                (holiday-solar-term "芒种" "芒种")
-                (holiday-solar-term "夏至" "夏至")
-                (holiday-solar-term "小暑" "小暑")
-                (holiday-solar-term "大暑" "大暑")
-                (holiday-solar-term "立秋" "立秋")
-                (holiday-solar-term "处暑" "处暑")
-                (holiday-solar-term "白露" "白露")
-                (holiday-solar-term "秋分" "秋分")
-                (holiday-solar-term "寒露" "寒露")
-                (holiday-solar-term "霜降" "霜降")
-                (holiday-solar-term "立冬" "立冬")
-                (holiday-solar-term "小雪" "小雪")
-                (holiday-solar-term "大雪" "大雪")
-                (holiday-solar-term "冬至" "冬至")
-                (holiday-lunar 5 5 "端午节" 0)
-                (holiday-lunar 8 15 "中秋节" 0)
-                (holiday-lunar 7 7 "七夕情人节" 0)
-                (holiday-lunar 12 8 "腊八节" 0)
-                (holiday-lunar 9 9 "重阳节" 0)
-                (holiday-lunar 12 22 "冬至" 0)))
-        (setq calendar-holidays eh-calendar-holidays)))
+      (setq eh-calendar-holidays
+            '(;;公历节日
+              (holiday-fixed 1 1 "元旦")
+              (holiday-fixed 2 14 "情人节")
+              (holiday-fixed 3 8 "妇女节")
+              (holiday-fixed 3 14 "白色情人节")
+              (holiday-fixed 4 1 "愚人节")
+              (holiday-fixed 5 1 "劳动节")
+              (holiday-fixed 5 4 "青年节")
+              (holiday-float 5 0 2 "母亲节")
+              (holiday-fixed 6 1 "儿童节")
+              (holiday-float 6 0 3 "父亲节")
+              (holiday-fixed 9 10 "教师节")
+              (holiday-fixed 10 1 "国庆节")
+              (holiday-fixed 10 24 "程序员节")
+              (holiday-fixed 12 25 "圣诞节")
+              ;; 农历节日
+              (holiday-lunar 1 1 "春节" 0)
+              (holiday-lunar 1 2 "春节" 0)
+              (holiday-lunar 1 3 "春节" 0)
+              (holiday-lunar 1 15 "元宵节" 0)
+              (holiday-solar-term "清明" "清明节")
+              (holiday-solar-term "小寒" "小寒")
+              (holiday-solar-term "大寒" "大寒")
+              (holiday-solar-term "立春" "立春")
+              (holiday-solar-term "雨水" "雨水")
+              (holiday-solar-term "惊蛰" "惊蛰")
+              (holiday-solar-term "春分" "春分")
+              (holiday-solar-term "谷雨" "谷雨")
+              (holiday-solar-term "立夏" "立夏")
+              (holiday-solar-term "小满" "小满")
+              (holiday-solar-term "芒种" "芒种")
+              (holiday-solar-term "夏至" "夏至")
+              (holiday-solar-term "小暑" "小暑")
+              (holiday-solar-term "大暑" "大暑")
+              (holiday-solar-term "立秋" "立秋")
+              (holiday-solar-term "处暑" "处暑")
+              (holiday-solar-term "白露" "白露")
+              (holiday-solar-term "秋分" "秋分")
+              (holiday-solar-term "寒露" "寒露")
+              (holiday-solar-term "霜降" "霜降")
+              (holiday-solar-term "立冬" "立冬")
+              (holiday-solar-term "小雪" "小雪")
+              (holiday-solar-term "大雪" "大雪")
+              (holiday-solar-term "冬至" "冬至")
+              (holiday-lunar 5 5 "端午节" 0)
+              (holiday-lunar 8 15 "中秋节" 0)
+              (holiday-lunar 7 7 "七夕情人节" 0)
+              (holiday-lunar 12 8 "腊八节" 0)
+              (holiday-lunar 9 9 "重阳节" 0)
+              (holiday-lunar 12 22 "冬至" 0)))
+      (setq calendar-holidays eh-calendar-holidays)))
 
-    (use-package calendar
-      :ensure nil
-      :config
-      (setq calendar-week-start-day 0) ; 一周第一天，0表示星期天, 1表示星期一
-      (setq calendar-month-name-array
-            ["一月" "二月" "三月" "四月" "五月" "六月"
-             "七月" "八月" "九月" "十月" "十一月" "十二月"])
-      (setq calendar-day-name-array
-            ["周日" "周一" "周二" "周三" "周四" "周五" "周六"])
+  (use-package calendar
+    :ensure nil
+    :config
+    (setq calendar-week-start-day 0) ; 一周第一天，0表示星期天, 1表示星期一
+    (setq calendar-month-name-array
+          ["一月" "二月" "三月" "四月" "五月" "六月"
+           "七月" "八月" "九月" "十月" "十一月" "十二月"])
+    (setq calendar-day-name-array
+          ["周日" "周一" "周二" "周三" "周四" "周五" "周六"])
 
-      (defun eh-org-chinese-anniversary (year lunar-month lunar-day &optional mark)
-        (if year
-            (let* ((d-date (diary-make-date lunar-month lunar-day year))
-                   (a-date (calendar-absolute-from-gregorian d-date))
-                   (c-date (calendar-chinese-from-absolute a-date))
-                   (cycle (car c-date))
-                   (yy (cadr c-date))
-                   (y (+ (* 100 cycle) yy)))
-              (diary-chinese-anniversary lunar-month lunar-day y mark))
-          (diary-chinese-anniversary lunar-month lunar-day year mark))))
+    (defun eh-org-chinese-anniversary (year lunar-month lunar-day &optional mark)
+      (if year
+          (let* ((d-date (diary-make-date lunar-month lunar-day year))
+                 (a-date (calendar-absolute-from-gregorian d-date))
+                 (c-date (calendar-chinese-from-absolute a-date))
+                 (cycle (car c-date))
+                 (yy (cadr c-date))
+                 (y (+ (* 100 cycle) yy)))
+            (diary-chinese-anniversary lunar-month lunar-day y mark))
+        (diary-chinese-anniversary lunar-month lunar-day year mark))))
 
-    (use-package org-capture
-      :ensure nil
-      :bind (("C-c c" . org-capture))
-      :config
-      (setq org-capture-templates
-            (let ((gtd-file (concat (file-name-as-directory eh-org-directory)
-                                    "i-gtd" "@" (substring (md5 system-name) 0 3)
-                                    ".org"))
-                  (other-file (concat (file-name-as-directory eh-org-directory)
-                                      "i-other.org")))
-              `(("n" "Note" entry (file ,gtd-file)
-                 "* %?\n%i\n %a")
-                ("a" "Appointment" entry (file ,gtd-file)
-                 "* %?\n  %t\n%i\n %a")
-                ("s" "Schedule" entry (file ,gtd-file)
-                 "* TODO %?\nSCHEDULED: %t\n%i\n %a")
-                ("k" "Schedule" entry (file ,gtd-file)
-                 "* TODO %?\nSCHEDULED: %t\n%i\n %a")
-                ("d" "Deadline" entry (file ,gtd-file)
-                 "* TODO %?\nDEADLINE: %t\n%i\n %a")
-                ("t" "TODO" entry (file ,gtd-file)
-                 "* TODO %?\n%i\n %a")
-                ("A" "Anniversary" plain (file+headline ,other-file "阳历生日")
-                 "\%\%%(or \"(org-anniversary 1985 4 17)\") 今天是%?%d岁阳历生日")
-                ("C" "Chinese Anniversary" plain (file+headline ,other-file "农历生日")
-                 "\%\%%(or \"(eh-org-chinese-anniversary 1985 4 17)\") 今天是%?%d岁农历生日")
-                ("D" "Diary" plain (file+headline ,other-file "节假日")
-                 "\%\%%(or \"(org-calendar-holiday)\")"))))))
+  (use-package org-capture
+    :ensure nil
+    :bind (("C-c c" . org-capture))
+    :config
+    (setq org-capture-templates
+          (let ((gtd-file (concat (file-name-as-directory eh-org-directory)
+                                  "i-gtd" "@" (substring (md5 system-name) 0 3)
+                                  ".org")))
+            `(("n" "Note" entry (file ,gtd-file)
+               "* %?\n%i\n %a")
+              ("a" "Appointment" entry (file ,gtd-file)
+               "* %?\n  %t\n%i\n %a")
+              ("s" "Schedule" entry (file ,gtd-file)
+               "* TODO %?\nSCHEDULED: %t\n%i\n %a")
+              ("k" "Schedule" entry (file ,gtd-file)
+               "* TODO %?\nSCHEDULED: %t\n%i\n %a")
+              ("d" "Deadline" entry (file ,gtd-file)
+               "* TODO %?\nDEADLINE: %t\n%i\n %a")
+              ("t" "TODO" entry (file ,gtd-file)
+               "* TODO %?\n%i\n %a")))))
 
   (use-package ob-core
     :ensure  nil
