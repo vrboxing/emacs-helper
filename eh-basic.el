410540bd15e1ac410d2c41f76d7c870a1736ae49
Feng Shu <tumashu@163.com>
2017-11-18 07:15:10 +0800

    simple eh-basic.el

--- eh-basic.el
+++ eh-basic.el
@@ -277,20 +277,11 @@
 
 ;; ** 设置 swiper 和 ivy-mode
 (use-package swiper
+  :bind (;; I use "C-x C-f" to open file, so bind "C-f" to
+         ;; `ivy-immediate-done' is very useful.
+         :map ivy-minibuffer-map
+              ("C-f" . ivy-immediate-done))
   :config
-
-  (use-package counsel
-    :bind
-    (("C-c C-r" . ivy-resume)
-     ("M-x" . counsel-M-x)
-     ("C-x b" . ivy-switch-buffer)
-     ("C-x f" . counsel-recentf)
-     ("C-x C-b" . ivy-switch-buffer)
-     ("C-x C-f" . counsel-find-file)
-     ("C-h f" . counsel-describe-function)
-     ("C-h v" . counsel-describe-variable)
-     ("C-c y" . counsel-yank-pop)))
-
   (ivy-mode 1)
   (setq ivy-count-format ""
         ivy-use-virtual-buffers t
@@ -298,10 +289,19 @@
         ivy-display-style 'fancy)
   (push '(counsel-M-x . "") ivy-initial-inputs-alist)
   (push '(counsel-describe-function . "") ivy-initial-inputs-alist)
-  (push '(counsel-describe-variable . "") ivy-initial-inputs-alist)
-  ;; I use "C-x C-f" to open file, so bind "C-f" to
-  ;; `ivy-immediate-done' is very useful.
-  (define-key ivy-minibuffer-map (kbd "C-f") #'ivy-immediate-done))
+  (push '(counsel-describe-variable . "") ivy-initial-inputs-alist))
+
+(use-package counsel
+  :bind
+  (("C-c C-r" . ivy-resume)
+   ("M-x" . counsel-M-x)
+   ("C-x b" . ivy-switch-buffer)
+   ("C-x f" . counsel-recentf)
+   ("C-x C-b" . ivy-switch-buffer)
+   ("C-x C-f" . counsel-find-file)
+   ("C-h f" . counsel-describe-function)
+   ("C-h v" . counsel-describe-variable)
+   ("C-c y" . counsel-yank-pop)))
 
 ;; * Footer
 (provide 'eh-basic)
