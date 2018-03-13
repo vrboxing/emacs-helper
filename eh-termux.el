617a7ca521e2f8b72519a9d21da788ca7bd3bd85
Feng Shu <tumashu@163.com>
2018-03-13 08:47:20 +0800

    Update eh-termux

--- eh-termux.el
+++ eh-termux.el
@@ -37,42 +37,42 @@
 
 
 (defun eh-termux-mode-line ()
-  `(" "
-    (propertize
-     "[切换]"
-     'mouse-face 'mode-line-highlight
-     'keymap
-     (let ((map (make-sparse-keymap)))
-       (define-key map [mode-line mouse-1] 'next-buffer)
-       map))
-    " "
-    (propertize
-     "[最大]"
-     'mouse-face 'mode-line-highlight
-     'keymap
-     (let ((map (make-sparse-keymap)))
-       (define-key map [mode-line mouse-1] 'delete-other-windows)
-       map))
-    " "
-    ,(when (buffer-file-name)
-       (list (propertize
-              "[保存]"
-              'mouse-face 'mode-line-highlight
-              'keymap
-              (let ((map (make-sparse-keymap)))
-                (define-key map [mode-line mouse-1] 'save-buffer)
-                map))
-             " "))
-    ,(when (eq major-mode 'org-mode)
-       (list (propertize
-              "[C-c C-c]"
-              'mouse-face 'mode-line-highlight
-              'keymap
-              (let ((map (make-sparse-keymap)))
-                (define-key map [mode-line mouse-1] 'org-ctrl-c-ctrl-c)
-                map))
-             " "))
-    "%b"))
+  (list " "
+        (propertize
+         "[切换]"
+         'mouse-face 'mode-line-highlight
+         'keymap
+         (let ((map (make-sparse-keymap)))
+           (define-key map [mode-line mouse-1] 'next-buffer)
+           map))
+        " "
+        (propertize
+         "[最大]"
+         'mouse-face 'mode-line-highlight
+         'keymap
+         (let ((map (make-sparse-keymap)))
+           (define-key map [mode-line mouse-1] 'delete-other-windows)
+           map))
+        " "
+        (when (buffer-file-name)
+          (list (propertize
+                 "[保存]"
+                 'mouse-face 'mode-line-highlight
+                 'keymap
+                 (let ((map (make-sparse-keymap)))
+                   (define-key map [mode-line mouse-1] 'save-buffer)
+                   map))
+                " "))
+        (when (eq major-mode 'org-mode)
+          (list (propertize
+                 "[C-c C-c]"
+                 'mouse-face 'mode-line-highlight
+                 'keymap
+                 (let ((map (make-sparse-keymap)))
+                   (define-key map [mode-line mouse-1] 'org-ctrl-c-ctrl-c)
+                   map))
+                " "))
+        "%b"))
 
 (defun eh-termux-enable ()
   (interactive)
