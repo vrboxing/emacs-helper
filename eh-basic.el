66b83904a971c19a84e2e4b67ed0c3fd1a5bc624
Feng Shu <tumashu@163.com>
2018-05-16 06:38:26 +0800

    update

--- eh-basic.el
+++ eh-basic.el
@@ -306,6 +306,8 @@
          ;; I use "C-x C-f" to open file, so bind "C-f" to
          ;; `ivy-immediate-done' is very useful.
          ("C-f" . ivy-immediate-done)
+         ([mouse-1] . ignore)
+         ([mouse-3] . ignore)
          ([mouse-4] . ivy-next-line)
          ([mouse-5] . ivy-previous-line))
   :config
